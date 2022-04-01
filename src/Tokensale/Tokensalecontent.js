f8fca50f3b53e05ac78661641832fe0b682a1655
ravinders590 <ravinders590@gmail.com>
2022-04-01 10:51:43 +0530

    Token update

--- src/Tokensale/Tokensalecontent.js
+++ src/Tokensale/Tokensalecontent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Tokensalecontent = ({TokenData}) => {
 
-    const [token,setToken] = useState('0x7Ce280570D3cbbCCa88238995466a503C73AC642');
+    const [token,setToken] = useState('0x2aCa5f90F2f9C3654931797ed3717eCC53c443b8');
 
     const copyToClipboard = ()=>{
           
@@ -36,7 +36,7 @@ const Tokensalecontent = ({TokenData}) => {
     }
         <div className="col-lg-12 col-xs-12">
             <div className="tokken-address">
-                <label htmlFor="tokken-link">Tokken Address:</label>
+                <label htmlFor="tokken-link">Token Address:</label>
                 <input id="tokken-link" type="text" className="form-control" defaultValue={token} placeholder="Tokken Address.." />
                 <button onClick={copyToClipboard} className="btn btn-primary">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-copy">
